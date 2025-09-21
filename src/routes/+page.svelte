bf7c4727876ce83d3fd889efc7cb61906a3c860d
kareemeldahshoury <eldah001@umn.edu>
2025-09-21 12:03:29 -0500

    color of enter button changed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -64,7 +64,7 @@
     class="w-full max-w-xs 
            px-4 py-3 
             md:text-lg 
-           bg-secondary-text hover:bg-secondary-text/80 text-custom-yellow
+           bg-secondary-text hover:bg-secondary-text/80 text-bg-white
            rounded-lg 
            focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 
            shadow-sm 
